824192c5313cfaf7cf9d637019bcebff89922123
Elizabeth Ptack <101746864+EPtackArts@users.noreply.github.com>
2022-12-07 21:22:27 -0600

    runs on correct port - working backend

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const db = require("../models")
+const db = require("../models/user.js")
 
 const { User } = db
 
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,21 +1,3 @@
-const {
-    Model
-  } = require('sequelize');
-  module.exports = (sequelize, DataTypes) => {
-    User.init({
-        userId: {
-          type: DataTypes.SMALLINT,
-          primaryKey: true,
-          autoIncrement: true
-        },
-        firstName: DataTypes.STRING,
-        lastName: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password: DataTypes.STRING
-      }, {
-        sequelize,
-        underscored: true,
-        modelName: 'User',
-      });
-      return User;
-    };
+module.exports = [{
+    
+}]
