56b0214353c473108c066eec6ed30520736de584
Elizabeth Ptack <101746864+EPtackArts@users.noreply.github.com>
2022-12-08 18:01:47 -0600

    method override

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@
 require('dotenv').config
 const express = require('express')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const app = express()
 
 //express settings
@@ -10,6 +11,8 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true })) 
+app.use(methodOverride('_method'))
+app.use(bodyParser.json())
 
 //routes
 app.use('/api/users', require('./controllers/users'))
