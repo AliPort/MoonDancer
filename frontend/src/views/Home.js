3fbaedf9dfeba6f664a5fa39089f4289a4498389
wms09291963 <98226872+wms09291963@users.noreply.github.com>
2022-12-10 14:31:24 -0500

    fixed image problem on home page

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,17 +1,20 @@
 import React, { Fragment } from "react";
-import beached from '../assets/beached.jpg';
+import beached from '../assets/beached.JPG';
 
-
-
-const Home = () => (
-  return <img src={beached} alt="boat next to shoreline" />;  
-  <Fragment>
+const Home = () => {
+ 
+  return (
+    <Fragment>
       <h1>HOME</h1>
       <h2>MoonDancer Boat Charters</h2>
-        <p>Enjoy the fishing trip  of a lifetime in beautiful Apalachee Bay, Port of Panacea, Florida! This incredible fishing destination offers some of the finest eating fish and a thrill you won't forget. From Apalachee Bay to AlligatorPoint to the deep blue ocean we have the knowledge and experience to make your fishing dreams come true ~ Moondancer
-</p>
+      <img src={beached} alt="boat next to shoreline" />; 
+        <p>Enjoy the fishing trip  of a lifetime in beautiful Apalachee Bay, Port of Panacea, Florida! This incredible fishing destination offers some of the finest eating fish and a thrill you wont forget. From Apalachee Bay to AlligatorPoint to the deep blue ocean we have the knowledge and experience to make your fishing dreams come true ~ Moondancer</p>
 
   </Fragment>
-);
+
+  )
+  
+  
+  };
 
 export default Home;
