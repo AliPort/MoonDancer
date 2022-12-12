c259f623046a080bc467a3577c6c37a0cfbc6ccf
wms09291963 <98226872+wms09291963@users.noreply.github.com>
2022-12-12 18:37:13 -0500

    added css to app.css, started form in Footer.js, added html to Home.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,60 +1,35 @@
 import React from "react";
+import moondancerlogo from '../assets/MoonDancerlogo.jpg';
+
+
 
 const Footer = () => (
   <footer className="bg-light p-3 text-center">
     <div className="logo" />
+    <img src={moondancerlogo}  alt="company logo" />; 
     <h1>MoonDancer Boat Charters</h1>
-   <h1>Ask the Captain </h1>
-
-   <form>
-  <label>
-    Name:
-    <input type="text" name="name" />
-  </label>
-  <label> 
-    Address:
-    <input type="text" address="address" />
-  </label>
-  <label>
-    City:
-    <input type="text" city="city" />
-  </label>
-  <label>
-    State:
-    <select>
-  <option value="al">AL</option>
-  <option value="ak">AK</option>
-  <option value="az">AZ</option>
-  <option value="ar">AR</option>
-  <option value="ca">CA</option>
-  <option value="co">CO</option>
-  <option value="ct">CT</option>
-  <option value="de">DE</option>
-  <option value="fl">FL</option>
-  <option value="ga">GA</option>
-  <option value="hi">HI</option>
-  <option value="id">ID</option>
-  <option value="il">IL</option>
-  <option value="in">IN</option>
-  <option value="ia">IA</option>
-  <option value="ks">KS</option>
-  <option value="ky">KY</option>
-  <option value="la">LA</option>
-  <option value="me">ME</option>
-  <option value="md">MD</option>
-  <option value="ma">MA</option>
-
-
-
+    <h1>Ask the Captain</h1>
+   
   
-</select>
-    <input type="text" state="state" />
+  <form>
+   <label>
+    First Name:
+    <input type="text" name="name" />
+   </label>
+   <label> 
+    Last Name:
+    <input type="text" address="address" />
+   </label>
+   <label>
+    Email*:
+    <input type="email" email="email" />
   </label>
-
+  <label>
+    Subject:
+    <input type="text" subject="subject" />
+  </label>
   <input type="submit" value="Submit" />
 </form>
-
-
     <p>
       Sample project provided by <a href="https://auth0.com">Auth0</a>
     </p>
--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -7,12 +7,15 @@ const Home = () => {
   return (
     <Fragment>
       <h1>HOME</h1>
-      <h2>MoonDancer Boat Charters</h2>
-      <img src={beached} alt="boat next to shoreline" />; 
-        <p>Enjoy the fishing trip  of a lifetime in beautiful Apalachee Bay, Port of Panacea, Florida! This incredible fishing destination offers some of the finest eating fish and a thrill you wont forget. From Apalachee Bay to AlligatorPoint to the deep blue ocean we have the knowledge and experience to make your fishing dreams come true ~ Moondancer</p>
+      <div>
+        <h2>MoonDancer Boat Charters</h2>
+        
+         <p>Enjoy the fishing trip  of a lifetime in beautiful Apalachee Bay, Port of Panacea, Florida! This incredible fishing destination offers some of the finest eating fish and a thrill you wont forget. From Apalachee Bay to AlligatorPoint to the deep blue ocean we have the knowledge and experience to make your fishing dreams come true ~ Moondancer
+         </p>
+      </div>
 
         
-  </Fragment>
+    </Fragment>
 
 
   )
