287fd7cadf61ef4d2887697cbc751dfe686ab237
Elizabeth Ptack <101746864+EPtackArts@users.noreply.github.com>
2022-12-17 17:29:21 -0600

    nav, home, and footer are styled

--- frontend/src/components/AskTheCaptain.js
+++ frontend/src/components/AskTheCaptain.js
@@ -2,12 +2,11 @@ function AskTheCaptain() {
     return (
     <div id="Ask">
         <form id="AskForm">
-            <h3>Ask the Captain</h3>
             <input className="AskInput" type="text" name="fname" placeholder="First Name" required/><br/>
             <input className="AskInput" type="text" name="lname" placeholder="Last Name" required/><br/>
             <input className="AskInput" type="email" name="email" placeholder="E-Mail" required/><br/>
             <input className="AskInput" type="text" name="subject" placeholder="Subject" required/><br/>
-            <input className="AskInput" type="text" name="message" placeholder="Your Message" required/><br/>
+            <input id="messageField" className="AskInput" type="text" name="message" placeholder="Your Message" required/><br/>
             <input className="AskInput" type="submit" value="Submit" />
         </form>
     </div>
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,22 +4,50 @@ import AskTheCaptain from "./AskTheCaptain";
 import Icon from '../assets/moondancericon.png'
 
 const Footer = () => (
-  <footer className="text-center text-lg-start bg-white text-muted">
+  <div className="footerDiv">
+  <footer className="text-center text-lg-start bg-light text-muted">
     {/* FOOTER COLUMNS */}
     <section className="">
-      <div className="container text-center text-md-start mt-5">
+      <div className="container text-center text-md-start mt-3">
         <div className="row mt-3">
           {/* ASK THE CAPTAIN */}
-          <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <div className="col-md-3 col-lg-4 col-xl-6 mx-auto mb-4">
             <h6 class="text-uppercase fw-bold mb-4">
-              <i class="fas fa-gem me-3"></i>MoonDancer
+              <i class="fas fa-gem me-3"></i>Ask Captain William
             </h6>
             <div id="AskDiv">
               <AskTheCaptain/>
             </div>
           </div>
-          {/* SITEMAP */}
-          <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          {/* CONTACT & WHILE YOU'RE IN TOWN */}
+          <div id="florida" class="col-md-3 col-lg-2 col-xl-3 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold mb-4">
+            Contact
+            </h6>
+            <p>
+              <a class="text-reset" target="blank">E-mail: </a>
+            </p>
+            <p>
+              <a class="text-reset" target="blank">Tel: </a>
+            </p>
+            <h6 class="text-uppercase fw-bold mb-4">
+            While You're Here
+            </h6>
+            <p>
+              <a href="https://www.visitflorida.com/" class="text-reset" target="blank">Visit Florida</a>
+            </p>
+            <p>
+              <a href="https://www.visitflorida.com/listing/wakulla-diving-center/29406/" class="text-reset" target="blank">Wakulla Diving Center</a>
+            </p>
+            <p>
+              <a href="https://crumsminimall.com/" class="text-reset" target="blank">Crum's Mini Mall</a>
+            </p>
+            <p>
+              <a href="https://gulfspecimen.org/" class="text-reset" target="blank">Gulf Specimen Marine Lab</a>
+            </p>
+          </div>
+          {/* SITE MAP & CREDITS */}
+          <div id="sitemap" class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
             <h6 className="text-uppercase fw-bold mb-4">
               SITEMAP
             </h6>
@@ -35,27 +63,7 @@ const Footer = () => (
             <p>
               <a className="text-reset" href="/fish">FISH INDEX</a><br/>
             </p>
-          </div>
-          {/* WHILE YOU'RE IN TOWN */}
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-            <h6 class="text-uppercase fw-bold mb-4">
-            While You're in Town
-            </h6>
-            <p>
-              <a href="https://www.visitflorida.com/" class="text-reset" target="blank">Visit Florida</a>
-            </p>
-            <p>
-              <a href="https://www.visitflorida.com/listing/wakulla-diving-center/29406/" class="text-reset" target="blank">Wakulla Diving Center</a>
-            </p>
-            <p>
-              <a href="https://crumsminimall.com/" class="text-reset" target="blank">Crum's Mini Mall</a>
-            </p>
-            <p>
-              <a href="https://gulfspecimen.org/" class="text-reset" target="blank">Gulf Specimen Marine Laboratories</a>
-            </p>
-          </div>
-          {/* CREDITS */}
-          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <br/>
             <h6 class="text-uppercase fw-bold mb-4">Credits</h6>
             <p>
               <a href="https://github.com/AliPort" target="blank">Alita Portillo</a>
@@ -75,11 +83,11 @@ const Footer = () => (
     </section>
     {/* FOOTER COPYRIGHT SECTION */}
     <div class="text-center p-4">
-      <img src={Icon}alt="moondancer logo icon"/>
-        © 2022 Copyright: MoonDancer LLC.
-        <a href='https://github.com/AliPort/MoonDancer' target="blank">Check out our code!<img id="github_logo" src={github} alt="github logo"/></a>
+      <img src={Icon}alt="moondancer logo icon" width="60px"/> © 2022 Copyright: MoonDancer Boat Charters - All Rights Reserved<br/>
+      Check out our code! <a href='https://github.com/AliPort/MoonDancer' target="blank"><img src={github} alt="github logo" width="40px"/></a>
     </div>
   </footer>
+  </div>
 );
 
 export default Footer;
