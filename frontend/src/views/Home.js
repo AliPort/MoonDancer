01fa3ae9428d4a302b3a9020fe8bdd530bf827a3
Elizabeth Ptack <101746864+EPtackArts@users.noreply.github.com>
2022-12-16 23:26:31 -0600

    booking info form is donegit add . needs to be connected to database and styled

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -6,8 +6,8 @@ const Home = () => {
   return (
     <Fragment>
       <h1>MoonDancer Boat Charters</h1>
-      <img class="banner_photo" src={beached} alt="boat next to shoreline" />
-      <a class="book_trip" href="/booking">BOOK YOUR TOUR TODAY</a>
+      <img className="banner_photo" src={beached} alt="boat next to shoreline" />
+      <a className="book_trip" href="/booking">BOOK YOUR TOUR TODAY</a>
       {/* I was thinking that the title and the button could be 
       laid out on top of the photo - similar to the Florida Sport Fishing site */}
       <p>Enjoy the fishing trip  of a lifetime in beautiful Apalachee Bay, Port of Panacea, Florida! This incredible fishing destination offers some of the finest eating fish and a thrill you wont forget. From Apalachee Bay to AlligatorPoint to the deep blue ocean we have the knowledge and experience to make your fishing dreams come true ~ Moondancer</p>
