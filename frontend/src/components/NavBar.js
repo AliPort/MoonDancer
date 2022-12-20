e42023d4bc300ac3f5aefaae4ad7268dfa516218
Elizabeth Ptack <101746864+EPtackArts@users.noreply.github.com>
2022-12-19 18:53:33 -0600

    fixed compiling errors

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { NavLink as RouterNavLink } from "react-router-dom";
 import Icon from '../assets/moondancericon.png';
 
--- frontend/src/views/Error404.js
+++ frontend/src/views/Error404.js
@@ -1,9 +1,12 @@
+import oops from "../assets/oops.jpeg"
+
 function Error404() {
     return (
-        <div className="container my-5">
+        <div className="oops">
             <h1>404: PAGE NOT FOUND</h1>
-            <p>Oops, sorry, we can't find this page!</p>
-            <img src="${process.env.REACT_APP_SERVER_URL}images/kittykat.jpeg" alt="cute cat pic" />
+            <p>looks like you're a fish out of water!
+            </p>
+            <img src={oops} alt="man in water holding a sign that says oops with shark looking at him." />
         </div>
     );
 }
